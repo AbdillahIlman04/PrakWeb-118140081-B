0b6e4d94b69a9dead7783ea3a34b6cb5574c26f1
AbdillahIlman04 <abdillahilman04@gmail.com>
2020-12-13 12:32:38 +0700

    test update branch Minggu-4

---
+++ CodeIniter/application/models/crud.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_crud extends CI_Model
+{
+
+    public function loginQuery($where)
+    {
+        return $this->db->select('*')->from("tbl_users")->where($where)->get();
+    }
+
+    public function insertUser($table, $data)
+    {
+        return $this->db->insert($table, $data);
+    }
+}
